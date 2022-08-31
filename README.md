3e032cba1a00010894445f5ccd9b2329dc14240b
Bernie Petters <bp4924@gmail.com>
2022-08-31 13:27:47 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# BP-Readme-Generator
+# README Generator 
+  ## Description 
+  Create a professional README.md file based on user inputs 
+  ## Installation 
+  Clone the repo to a new folder. In the terminal navigate to this folder  
+  ## Usage 
+  Run node index.js in this new folder 
+  ## License 
+  none 
+  ## Contributors 
+  none 
+  ## GitHub 
+  none 
+  ## LinkedIn 
+  none
---
+++ index.js
@@ -0,0 +1,74 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
+
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "Project Title:",
+  },
+  {
+    type: "input",
+    name: "discription",
+    message: "Project Description:",
+  },
+  {
+    type: "input",
+    name: "install",
+    message: "Installation instructions:",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Usage:",
+  },
+  {
+    type: "input",
+    name: "license",
+    message: "Choose license",
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "Contributors:",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "Tests:",
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "Enter your GitHub Username",
+  },
+  {
+    type: "input",
+    name: "linkedin",
+    message: "Enter your LinkedIn URL.",
+  },
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, answers) {
+  console.log(answers);
+  fs.writeFile(fileName, answers, (err) =>
+    err ? console.log(err) : console.log("Successfully created README.md!")
+  );
+}
+
+// TODO: Create a function to initialize app
+function init() {
+  console.log("init function");
+}
+
+// Function call to initialize app
+init();
+
+inquirer.prompt(questions).then((answers) => {
+  const fileName = "README.md";
+  const readmePageContent = generateMarkdown(answers);
+
+  writeToFile(fileName, readmePageContent);
+});
