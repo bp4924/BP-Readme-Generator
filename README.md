5f1505002de5c55cf7388f6e029839edb151b0da
Bernie Petters <bp4924@gmail.com>
2022-08-31 15:49:41 -0400

    license text and URL hard-code

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
-# README Generator 
+# aa 
   ## Description 
-  Create a professional README.md file based on user inputs 
+  ss 
   ## Installation 
-  Clone the repo to a new folder. In the terminal navigate to this folder  
+  gg  
   ## Usage 
-  Run node index.js in this new folder 
+  ee 
   ## License 
-  none 
+  Copyright (c) 2022. All rights reserved.
+
+  Licensed under the bb license https://choosealicense.com/licenses/mit/
   ## Contributors 
-  none 
+  dd 
   ## GitHub 
-  none 
+  hh 
   ## LinkedIn 
-  none
+  hh
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,17 +1,34 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
+function renderLicenseBadge(license) {
+  console.log(license, " badge");
+}
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
+function renderLicenseLink(license) {
+  console.log(license, " link");
+  const link = "https://choosealicense.com/licenses/mit/";
+  console.log(link);
+  const licenseURL = link;
+  console.log(licenseURL);
+  return licenseURL;
+}
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  const licenseText = `Copyright (c) 2022. All rights reserved.
+
+  Licensed under the ${license} license`;
+  //  console.log(licenseText);
+  return licenseText;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  const licenseBadge = renderLicenseBadge(data.license);
+  const licenseLink = renderLicenseLink(data.license);
+  const licenseSection = renderLicenseSection(data.license);
+
   const readMeText = `# ${data.title} 
   ## Description 
   ${data.discription} 
@@ -20,7 +37,7 @@ function generateMarkdown(data) {
   ## Usage 
   ${data.usage} 
   ## License 
-  ${data.license} 
+  ${licenseSection} ${licenseLink}
   ## Contributors 
   ${data.contributing} 
   ## GitHub 
@@ -28,15 +45,6 @@ function generateMarkdown(data) {
   ## LinkedIn 
   ${data.linkedin}`;
   return readMeText;
-
-  // return `# ${data.title} #
-  // Description ${data.discription} #
-  // Install ${data.install}  #
-  // Usage ${data.usage} #
-  // License ${data.license} #
-  // Contributors ${data.contributing} #
-  // GitHub ${data.github} #
-  // LinkedIn ${data.linkedin}`;
 }
 
 module.exports = generateMarkdown;
