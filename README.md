1e88d2c139241ef76bd074430469163aa3025f7d
Bernie Petters <bp4924@gmail.com>
2022-09-07 10:23:42 -0400

    shields

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
-[<svg width="104" height="20" role="img" aria-label="License: GNU LGPL"><title>License: GNU LGPL</title><linearGradient id="s" x2="0" y2="100%"><stop offset="0" stop-color="#bbb" stop-opacity=".1"/><stop offset="1" stop-opacity=".1"/></linearGradient><clipPath id="r"><rect width="104" height="20" rx="3" fill="#fff"/></clipPath><g clip-path="url(#r)"><rect width="51" height="20" fill="#555"/><rect x="51" width="53" height="20" fill="#4c1"/><rect width="104" height="20" fill="url(#s)"/></g><g fill="#fff" text-anchor="middle" font-family="Verdana,Geneva,DejaVu Sans,sans-serif" text-rendering="geometricPrecision" font-size="110"><text aria-hidden="true" x="265" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="410">License</text><text x="265" y="140" transform="scale(.1)" fill="#fff" textLength="410">License</text><text aria-hidden="true" x="785" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="470">GNU LGPL</text><text x="785" y="140" transform="scale(.1)" fill="#fff" textLength="470">GNU LGPL</text></g></svg>](https://choosealicense.com/licenses/lgpl-3.0?sanitize=true)
+[![License: GPL-3.0}](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://choosealicense.com/licenses/gpl-3.0/)
 
-# Generate Readme
+# g
 
 ---
 
 ## Table of Contents
 
-- [Generate Readme](#generate-readme)
+- [g](#g)
   - [Table of Contents](#table-of-contents)
   - [Description](#description)
   - [Installation](#installation)
@@ -18,35 +18,35 @@
 
 ## Description
 
-      Create a high-quality, professional README.md file based on user responses
+      d
 
 ## Installation
 
-      Clone this repository to a new folder. Type 'npm i' to create the needed node_modules folder and files
+      s
 
 ## Usage
 
-      Type 'node index.js' in the terminal and respond to the prompts
+      i
 
 ## License
 
-Licensed under the [GNU LGPL](https://choosealicense.com/licenses/lgpl-3.0/) license;
+Licensed under the [GNU GPL](https://choosealicense.com/licenses/gpl-3.0/) license;
 
 Copyright © 2022. All rights reserved.
 
 ## Tests
 
-      None
+      d
 
 ## Contributing
 
-      Push suggested changes to a new branch on gitHub for review, or contact me through the links below
+      p
 
 ## Questions
 
 - ### email
-  <a href="mailTo: bp4924@gmail.com?subject=Hello!" alt="" >bp4924@gmail.com</a>
+  <a href="mailTo: b?subject=Hello!" alt="" >b</a>
 - ### GitHub
-  [bp4924](https://github.com/bp4924)
+  [d](https://github.com/d)
 - ### LinkedIn
-  [berniepetters](https://linkedin.com/in/berniepetters)
+  [s](https://linkedin.com/in/s)
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,13 +1,18 @@
+let urlTag;
+
 // license badge
-function renderLicenseBadge(license, licenseLink) {
-  const licenseBadge = `[<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="104" height="20" role="img" aria-label="License: ${license}"><title>License: ${license}</title><linearGradient id="s" x2="0" y2="100%"><stop offset="0" stop-color="#bbb" stop-opacity=".1"/><stop offset="1" stop-opacity=".1"/></linearGradient><clipPath id="r"><rect width="104" height="20" rx="3" fill="#fff"/></clipPath><g clip-path="url(#r)"><rect width="51" height="20" fill="#555"/><rect x="51" width="53" height="20" fill="#4c1"/><rect width="104" height="20" fill="url(#s)"/></g><g fill="#fff" text-anchor="middle" font-family="Verdana,Geneva,DejaVu Sans,sans-serif" text-rendering="geometricPrecision" font-size="110"><text aria-hidden="true" x="265" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="410">License</text><text x="265" y="140" transform="scale(.1)" fill="#fff" textLength="410">License</text><text aria-hidden="true" x="785" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="470">${license}</text><text x="785" y="140" transform="scale(.1)" fill="#fff" textLength="470">${license}</text></g></svg>](${licenseLink})`;
+function renderLicenseBadge(urlTag, license, licenseLink) {
+  const urlTagToCap = urlTag.toUpperCase();
+  console.log(urlTagToCap);
+  const licenseBadge = `[![License: ${urlTagToCap}}](https://img.shields.io/badge/License-${license}-blue.svg)](${licenseLink})`;
+  // const licenseBadge = `[<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="104" height="20" role="img" aria-label="License: ${license}"><title>License: ${license}</title><linearGradient id="s" x2="0" y2="100%"><stop offset="0" stop-color="#bbb" stop-opacity=".1"/><stop offset="1" stop-opacity=".1"/></linearGradient><clipPath id="r"><rect width="104" height="20" rx="3" fill="#fff"/></clipPath><g clip-path="url(#r)"><rect width="51" height="20" fill="#555"/><rect x="51" width="53" height="20" fill="#4c1"/><rect width="104" height="20" fill="url(#s)"/></g><g fill="#fff" text-anchor="middle" font-family="Verdana,Geneva,DejaVu Sans,sans-serif" text-rendering="geometricPrecision" font-size="110"><text aria-hidden="true" x="265" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="410">License</text><text x="265" y="140" transform="scale(.1)" fill="#fff" textLength="410">License</text><text aria-hidden="true" x="785" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="470">${license}</text><text x="785" y="140" transform="scale(.1)" fill="#fff" textLength="470">${license}</text></g></svg>](${licenseLink})`;
   return licenseBadge;
 }
 
 // license functions
 function renderLicenseLink(license) {
   console.log(license, " link");
-  let urlTag;
+  //  let urlTag;
 
   switch (license) {
     case "MIT":
@@ -70,7 +75,7 @@ function renderLinkedInLink(linkedIn) {
 // generate markdown for README
 function generateMarkdown(data) {
   const licenseLink = renderLicenseLink(data.license);
-  const licenseBadge = renderLicenseBadge(data.license, licenseLink);
+  const licenseBadge = renderLicenseBadge(urlTag, licenseLink);
   const licenseSection = renderLicenseSection(data.license, licenseLink);
 
   const emailLink = renderEmailLink(data.email);
