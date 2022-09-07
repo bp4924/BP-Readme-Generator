b961a94c0d4e186b8d31a0b714cc7b95c9819dee
Bernie Petters <bp4924@gmail.com>
2022-09-07 09:18:52 -0400

    badge

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
+<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="104" height="20" role="img" aria-label="License: GNU LGPL"><title>License: GNU LGPL</title><linearGradient id="s" x2="0" y2="100%"><stop offset="0" stop-color="#bbb" stop-opacity=".1"/><stop offset="1" stop-opacity=".1"/></linearGradient><clipPath id="r"><rect width="104" height="20" rx="3" fill="#fff"/></clipPath><g clip-path="url(#r)"><rect width="51" height="20" fill="#555"/><rect x="51" width="53" height="20" fill="#4c1"/><rect width="104" height="20" fill="url(#s)"/></g><g fill="#fff" text-anchor="middle" font-family="Verdana,Geneva,DejaVu Sans,sans-serif" text-rendering="geometricPrecision" font-size="110"><text aria-hidden="true" x="265" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="410">License</text><text x="265" y="140" transform="scale(.1)" fill="#fff" textLength="410">License</text><text aria-hidden="true" x="785" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="470">GNU LGPL</text><text x="785" y="140" transform="scale(.1)" fill="#fff" textLength="470">GNU LGPL</text></g></svg>
+
 # Generate Readme
 
-[<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="104" height="20" role="img" aria-label="License: Apache"><title>License: Apache</title><linearGradient id="s" x2="0" y2="100%"><stop offset="0" stop-color="#bbb" stop-opacity=".1"/><stop offset="1" stop-opacity=".1"/></linearGradient><clipPath id="r"><rect width="104" height="20" rx="3" fill="#fff"/></clipPath><g clip-path="url(#r)"><rect width="51" height="20" fill="#555"/><rect x="51" width="53" height="20" fill="#4c1"/><rect width="104" height="20" fill="url(#s)"/></g><g fill="#fff" text-anchor="middle" font-family="Verdana,Geneva,DejaVu Sans,sans-serif" text-rendering="geometricPrecision" font-size="110"><text aria-hidden="true" x="265" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="410">License</text><text x="265" y="140" transform="scale(.1)" fill="#fff" textLength="410">License</text><text aria-hidden="true" x="785" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="470">Apache</text><text x="785" y="140" transform="scale(.1)" fill="#fff" textLength="470">Apache</text></g></svg>](https://choosealicense.com/licenses/apache-2.0/)
+---
 
 ## Table of Contents
 
@@ -20,7 +22,7 @@
 
 ## Installation
 
-      Clone to a new folder. Type 'npm i' to create the needed node_modules folder and files
+      Clone this repository to a new folder. Type 'npm i' to create the needed node_modules folder and files
 
 ## Usage
 
@@ -28,7 +30,7 @@
 
 ## License
 
-Licensed under the [Apache](https://choosealicense.com/licenses/apache-2.0/) license;
+Licensed under the [GNU LGPL](https://choosealicense.com/licenses/lgpl-3.0/) license;
 
 Copyright © 2022. All rights reserved.
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,24 +1,10 @@
-let section = "";
-
-const sections = [
-  "Description",
-  "Contents",
-  "Installation",
-  "Usage",
-  "License",
-  "Contributing",
-  "Tests",
-  "Questions",
-];
-
-// TODO: Create a function that returns a license badge based on which license is passed in'
-// If there is no license, return an empty string
+// license badge
 function renderLicenseBadge(license, licenseLink) {
   const licenseBadge = `[<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="104" height="20" role="img" aria-label="License: ${license}"><title>License: ${license}</title><linearGradient id="s" x2="0" y2="100%"><stop offset="0" stop-color="#bbb" stop-opacity=".1"/><stop offset="1" stop-opacity=".1"/></linearGradient><clipPath id="r"><rect width="104" height="20" rx="3" fill="#fff"/></clipPath><g clip-path="url(#r)"><rect width="51" height="20" fill="#555"/><rect x="51" width="53" height="20" fill="#4c1"/><rect width="104" height="20" fill="url(#s)"/></g><g fill="#fff" text-anchor="middle" font-family="Verdana,Geneva,DejaVu Sans,sans-serif" text-rendering="geometricPrecision" font-size="110"><text aria-hidden="true" x="265" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="410">License</text><text x="265" y="140" transform="scale(.1)" fill="#fff" textLength="410">License</text><text aria-hidden="true" x="785" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="470">${license}</text><text x="785" y="140" transform="scale(.1)" fill="#fff" textLength="470">${license}</text></g></svg>](${licenseLink})`;
   return licenseBadge;
 }
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
+
+// license functions
 function renderLicenseLink(license) {
   console.log(license, " link");
   let urlTag;
@@ -52,24 +38,20 @@ function renderLicenseLink(license) {
   }
 
   const licenseURL = `https://choosealicense.com/licenses/${urlTag}/`;
-  console.log(licenseURL);
   return licenseURL;
 }
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
+
 function renderLicenseSection(license, licenseURL) {
-  const licenseText = `
-  
+  const licenseText = `  
   Licensed under the [${license}](${licenseURL}) license;
   
   Copyright © 2022. All rights reserved.`;
-  //  console.log(licenseText);
   return licenseText;
 }
 
+// questions links
 function renderEmailLink(email) {
   const emailLink = `<a href="mailTo: ${email}?subject=Hello!" alt="" >${email}</a>`;
-  console.log(emailLink);
   return emailLink;
 }
 
@@ -85,15 +67,7 @@ function renderLinkedInLink(linkedIn) {
   return linkedInLink;
 }
 
-// function renderTableOfContents(sections) {
-//   for (let i = 0; i < sections.length; i++) {
-//     section += ` - [${sections[i]}](#${sections[i].toLowerCase()})`;
-//     console.log(section);
-//   }
-//   return section;
-// }
-
-// TODO: Create a function to generate markdown for README
+// generate markdown for README
 function generateMarkdown(data) {
   const licenseLink = renderLicenseLink(data.license);
   const licenseBadge = renderLicenseBadge(data.license, licenseLink);
@@ -102,7 +76,6 @@ function generateMarkdown(data) {
   const emailLink = renderEmailLink(data.email);
   const githubLink = renderGithubLink(data.github);
   const linkedInLink = renderLinkedInLink(data.linkedIn);
-  //  const tableOfContentsList = renderTableOfContents(sections);
 
   const readMeText = `${licenseBadge}
   # ${data.title}            
@@ -116,8 +89,6 @@ function generateMarkdown(data) {
   - [Contributing](#contributing)
   - [Questions](#questions)  
   
-  ======
-
   ## Description 
       ${data.discription} 
   ## Installation 
